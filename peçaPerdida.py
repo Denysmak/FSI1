03f3d549d911c186098c6065afc5e2c2921c3e43
Denys Makene <85587960+Denysmak@users.noreply.github.com>
2023-10-06 13:04:24 -0300

    Update peçaPerdida.py

--- peçaPerdida.py
+++ peçaPerdida.py
@@ -1,4 +1,4 @@
-#A quantidade de peças que deveriam ter no quebra cabeças
+#A quantidade de peças que deveriam existir no quebra cabeças
 entradaUm = int(input("Digite a primeira entrada: "))
 #A entrada que corresponde a todas as peças que temos do quebra cabeça
 entradaDois = input("Digite a segunda entrada: ")
@@ -9,7 +9,7 @@ listaComp = []
 #For loop que vai adicionar os valores entre n e 1 na lista completa
 for i in range(entradaUm):
     listaComp.append(i + 1)
-#For loop que vai remover os valores que existem na lista incompleta mas não existem na lista completa
+#For loop que vai remover da lista completa todos os valores existentes na lista incompleta
 for i in range(len(entradaDois)):
     if(entradaDois[i] != " "):
         listaComp.remove(int(entradaDois[i]))
